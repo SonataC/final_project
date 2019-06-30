303b2f8f601861760fab74c21bd7232bf2226b05
Sonata <sonata.cernikaite@gmail.com>
2019-06-30 15:49:40 +0300

    Added json file

--- js/final.js
+++ js/final.js
@@ -1,105 +1,8 @@
-(function ($) {
-    $.fn.countTo = function (options) {
-        options = options || {};
-
-        return $(this).each(function () {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-                from: $(this).data('from'),
-                to: $(this).data('to'),
-                speed: $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals: $(this).data('decimals')
-            }, options);
-
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
-
-            // references & variables that will change with each update
-            var self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
-
-            $self.data('countTo', data);
-
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
-            }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-            // initialize the element with the starting value
-            render(value);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-
-                render(value);
-
-                if (typeof (settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-
-                    if (typeof (settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-
-            function render(value) {
-                var formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue);
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0,               // the number the element should start at
-        to: 0,                 // the number the element should end at
-        speed: 1000,           // how long it should take to count between the target numbers
-        refreshInterval: 100,  // how often the element should be updated
-        decimals: 0,           // the number of decimal places to show
-        formatter: formatter,  // handler for formatting the value before rendering
-        onUpdate: null,        // callback method for every time the element is updated
-        onComplete: null       // callback method for when the element finishes updating
-    };
-
-    function formatter(value, settings) {
-        return value.toFixed(settings.decimals);
-    }
-}(jQuery));
-
-jQuery(function ($) {
-    // custom formatting example
-    $('.count-number').data('countToOptions', {
-        formatter: function (value, options) {
-            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-        }
-    });
-
-    // start all the timers
-    $('.timer').each(count);
-
-    function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
-    }
-});
-
+//on scroll animations
 $(function () {
     var scrollActions = {
         progressBars: false,
+        runningNumbers: false
     };
     $(window).on('scroll', function (e) {
         var _this = $(this);
@@ -108,23 +11,19 @@ $(function () {
         if (scrolledTop + _this.outerHeight() >= $("#progress_bars").offset().top
             && !scrollActions.progressBars
         ) {
-            startProgressBarsAnimation();
+            startProgressBars();
             scrollActions.progressBars = true;
         }
 
+        if (scrolledTop + _this.outerHeight() >= $("#running_numbers").offset().top
+            && !scrollActions.runningNumbers
+        ) {
+            startRunningNumbers();
+            scrollActions.runningNumbers = true;
+        }
     });
 
-    // jQuery example
-    // $('.progress-bar').each(function () {
-    //     var elm = $(this);
-    //
-    //     setTimeout(function () {
-    //         elm.animate({width:  parseFloat(elm.data('percentage')) + "%"}, 500);
-    //     }, 3000);
-    // });
-
-    // Plain JS
-    function startProgressBarsAnimation() {
+    function startProgressBars() {
         var elements = document.getElementsByClassName('progress-bar');
         for (var i = 0; i < elements.length; i++) {
             var elm = elements[i];
@@ -138,7 +37,7 @@ $(function () {
     }
 
     function handleText(elm, hide) {
-        var titleContainerElm = elm.getElementsByClassName("title-container")[0];
+        var titleContainerElm = elm.getElementsByClassName("title")[0];
         var left = 0;
         if (hide) {
             left = "-" + titleContainerElm.offsetWidth + "px";
@@ -147,7 +46,7 @@ $(function () {
     }
 
     function addInterval(elm) {
-        var innerCloudElm = elm.getElementsByClassName('inner-cloud')[0];
+        var innerCloudElm = elm.getElementsByClassName('inside-box')[0];
         var percentage = parseFloat(elm.getAttribute('data-percentage'));
         var counter = 0;
         var interval = setInterval(function () {
@@ -161,8 +60,108 @@ $(function () {
             counter++;
         }, 30);
     }
+
+    function startRunningNumbers() {
+
+        var arr = document.getElementsByClassName('running-number');
+
+        var increment = 0;
+        for (var i = 0; i < arr.length; i++) {
+            var elm = arr[i];
+            slideDown(elm, increment);
+
+            // Run the numbers
+            runTheNumbers(elm.getElementsByClassName("count")[0]);
+
+            increment += 500;
+        }
+    }
+
+    function runTheNumbers(elm) {
+        var counter = 0;
+        var interval = setInterval(function () {
+            elm.innerHTML = counter;
+
+            if (counter === parseInt(elm.getAttribute('data-max'))) {
+                stop(interval);
+            }
+            counter++;
+        }, .35);
+    }
+
+    function slideDown(elm, increment) {
+        setTimeout(function () {
+            elm.classList.remove("invisible");
+        }, increment);
+
+    }
 });
 
+//ajax and json PRICING PLANS
+startAjax();
+
+function startAjax() {
+    ajax('json/data.json', preparePlans);
+}
+
+function ajax(url, callBack) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            callBack(JSON.parse(this.responseText));
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function preparePlans(response) {
+    var html = '';
+    var plans = response.plans;
+
+    for (let i in plans) {
+        if (plans.hasOwnProperty(i)) {
+            let active = '';
+            if (plans[i].active) {
+                active = 'active';
+            }
+            html +=
+                '<div class="col-md-4 py-5 py-md-0">' +
+                '<table class="plan-table text-center table-bordered rounded">' +
+                '<thead>' +
+                '<tr>' +
+                '<th class="plan-header text-center position-relative ' + active + '">' +
+                '<div class="hexagon-container">' +
+                '<div class="hexagon mx-auto">' +
+                '<div class="hexagon-content">' +
+                '<h6 class="text-center price">' + plans[i].price + '</h6>' +
+                '<p class="periodicity text-center text-uppercase">' + plans[i].periodicity + '</p>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<p class="plan-name text-uppercase">' + plans[i].name + '</p>' +
+                '</th>' +
+                '</tr>' +
+                '</thead>' + //end of table header
+                '<tbody>';
+            for (let j in plans[i].benefits) {
+                if (plans[i].benefits.hasOwnProperty(j)) {
+                    let indicator = '<i class="fas fa-times px-2 text-danger"></i>';
+                    if (plans[i].benefits[j]) {
+                        indicator = '<i class="fas fa-check px-2 text-success"></i>';
+                    }
+                    html += '<tr><td class="text-capitalize">' +
+                        indicator + j + '</td></tr>'
+                }
+            }
+            html += '</tbody><tfoot><td class="py-3"><button class="btn btn-plan"> <i class="fas fa-cart-plus fa-xs text-secondary px-2">' +
+                '</i>Order now</button></td></tfoot></table></div>'
+        }
+    }
+    document.getElementById('plans_section').innerHTML = html;
+}
+
+//video
 $(function () {
     $(".video").click(function () {
         var theModal = $(this).data("target"),
